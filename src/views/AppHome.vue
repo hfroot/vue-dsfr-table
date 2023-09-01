f5eb1b4d95906bfbfa9ff253367972f010c67ce9
Helen Root <helenroot1@gmail.com>
2023-09-01 13:35:07 +0200

    Minimal refresh bug example

--- src/views/AppHome.vue
+++ src/views/AppHome.vue
@@ -1,3 +1,23 @@
 <template>
-  <h1>Accueil</h1>
+  <div>
+    <h1>Test DSFR table refresh</h1>
+    <DsfrTable :headers="headers" :rows="pets" />
+    <ul>
+      <li v-for="pet in pets" :key="pet[0]">{{ pet[0] }}</li>
+    </ul>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      headers: ["H1", "H2"],
+      pets: [["test", "example"]],
+    }
+  },
+  mounted() {
+    this.pets = [["new entry", "stuff"]]
+  },
+}
+</script>
